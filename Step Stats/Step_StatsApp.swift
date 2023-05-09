dcff5f6ec7c507653b4843eaa3bfe97a5c3e0db0
Michael Bryant <mcb115@duke.edu>
2023-05-08 21:16:02 -0400

    Initial Commit

---
+++ Step Stats/Step_StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_StatsApp.swift
+//  Step Stats
+//
+//  Created by Michael Bryant on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Step_StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
